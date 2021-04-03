b9237b13a7bc9e871cd1074fd343585875064992
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-04-03 12:07:02 -0300

    script aula 4

---
+++ scripts/4_tipos_de_objetos_no_R.R
@@ -0,0 +1,3 @@
+# tipos de objetos no R
+
+vetor <- c(1, 2, 3, 4, 5, 6) # vetor
--- scripts/exemplo1.R
+++
@@ -1,8 +0,0 @@
-# sumário da base de dados iris
-summary(iris)
-
-# plots da base de dados iris
-plot(iris)
-
-# estrutura da base de dados iris
-str(iris)
