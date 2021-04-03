82aea3b1b57976e5d3e667cdc2c81bda2969f43a
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-04-03 11:55:53 -0300

    scripts aula ppgcp 2021.01

---
+++ scripts/3.objetos_e_funcoes_no_R.R
@@ -0,0 +1,9 @@
+# criando objetos e funções no R
+
+vetor <- c(1, 2, 3, 5, 7) # criação de um vetor
+
+str(vetor) # estrutura do vetor
+
+regCarros <- lm(mpg ~ ., mtcars) # criação de um modelo de regressão
+
+str(regCarros) # estrutura da regressão
