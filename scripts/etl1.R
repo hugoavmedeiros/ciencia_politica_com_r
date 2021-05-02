e0696f719cd8312b946b393e69e00361223f273a
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-05-02 20:17:59 -0300

    extracao 3 - carga incremental

--- scripts/etl1.R
+++ scripts/etl1.R
@@ -1,3 +1,5 @@
+sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/3531bafe-d47d-415e-b154-a881081ac76c/download/acidentes-2019.csv', sep = ';', encoding = 'UTF-8')
+
 # carrega a base de snistros de transito do site da PCR
 sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/fc1c8460-0406-4fff-b51a-e79205d1f1ab/download/acidentes_2020-novo.csv', sep = ';', encoding = 'UTF-8')
 
