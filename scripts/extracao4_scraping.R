b450825b5546df3ba24f4db6b09d45e4294294a2
hugoavmedeiros <36339656+hugoavmedeiros@users.noreply.github.com>
2021-05-02 23:05:42 -0300

    extracao 2 e 4

---
+++ scripts/extracao4_scraping.R
@@ -0,0 +1,30 @@
+# arquivos html
+library(rvest)
+library(dplyr)
+
+# tabelas da wikipedia
+url <- "https://pt.wikipedia.org/wiki/%C3%93scar"
+
+urlTables <- url %>% read_html %>% html_nodes("table")
+
+urlLinks <- url %>% read_html %>% html_nodes("link")
+
+filmesPremiados <- as.data.frame(html_table(urlTables[5]))
+
+# resultados do brasileirão
+
+resultadosBrasileirao <- read_html("https://www.cbf.com.br")
+
+resultadosBrasileirao <- resultadosBrasileirao %>% html_nodes(".swiper-slide")
+
+rodada <- resultadosBrasileirao %>% html_nodes(".aside-header .text-center") %>% html_text()
+
+resultados <- resultadosBrasileirao %>% html_nodes(".aside-content .clearfix")
+
+mandante <- resultados %>% html_nodes(".pull-left .time-sigla") %>% html_text()
+
+visitante <- resultados %>% html_nodes(".pull-right .time-sigla") %>% html_text()
+
+tabelaBrasileirao <- data.frame(
+  mandante = mandante,
+  visitante = visitante)
