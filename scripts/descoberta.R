93690275a49ff3fa935d467922a00794fc7d08ef
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-05-23 22:20:03 -0300

    descoberta

---
+++ scripts/descoberta.R
@@ -0,0 +1,8 @@
+library(funModeling) 
+library(tidyverse) 
+
+glimpse(iris) # olhada nos dados
+status(iris) # estrutura dos dados (missing etc)
+freq(iris) # frequência das variáveis fator
+plot_num(iris) # exploração das variáveis numéricas
+profiling_num(iris) # estatísticas das variáveis numéricas
