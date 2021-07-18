8394ae0d7cae2fa192838fc964039435f24f2770
hugoavmedeiros <36339656+hugoavmedeiros@users.noreply.github.com>
2021-07-18 18:10:46 -0300

    Delete enriquecimento.R

--- scripts/enriquecimento.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-library(tidyverse)
-
-baseCodMun <- read.table('bases_originais/base_codigos_mun.txt', sep = ',', header = T, encoding = 'UTF-8')
-
-campusIES <- read.csv2('bases_originais/ies_georref.csv')
-
-campusIES <- left_join(campusIES, baseCodMun, by = c('CO_MUNICIPIO' = 'id_munic_7'))
