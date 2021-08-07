64b64f237f7bfd782913bbd69bb6fa895717581c
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-08-07 10:34:27 -0300

    atualização árvores e ENEM

--- scripts/machine_learning/arvores_decisao_ENEM.R
+++ scripts/machine_learning/arvores_decisao_ENEM.R
@@ -7,18 +7,18 @@ ENEM_ESCOLA_2019 <- read.csv2('bases_tratadas/ENEM_ESCOLA_2019.csv', stringsAsFa
 
 # AED 
 status(ENEM_ESCOLA_2019) # explorar a qualidade das variáveis
+freq(ENEM_ESCOLA_2019) # explorar os fatores
 
 # Pré-processamento
-particaoENEM = createDataPartition(1:nrow(ENEM_ESCOLA_2019),p=.7) # cria a partição 70-30
-treinoENEM = ENEM_ESCOLA_2019[particaoENEM$Resample1,] # treino
-testeENEM = ENEM_ESCOLA_2019[- particaoENEM$Resample1,] # - treino = teste
+particaoENEM = createDataPartition(1:nrow(ENEM_ESCOLA_2019), p=.7) # cria a partição 70-30
+treinoENEM = ENEM_ESCOLA_2019[particaoENEM$Resample1, ] # treino
+testeENEM = ENEM_ESCOLA_2019[-particaoENEM$Resample1, ] # - treino = teste
 
 # Mineração e predição com Árvores de Decisão
-
 cTreeENEM <- party::ctree(media ~ tipo + TDI_03 + MHA_03, treinoENEM) # árvore de decisão com inferência condicional
 plot(cTreeENEM) # plot
 
-forestENEM = randomForest(treinoENEM[,c(3, 8, 12)], treinoENEM[,4], ntree = 10, keep.forest=T, importance=T) # floresta aleatória
+forestENEM = randomForest(treinoENEM[ , c(3, 8, 12)], treinoENEM[ , 4], ntree = 10, keep.forest=T, importance=T) # floresta aleatória
 
 permimp(forestENEM) # importância de cada variável
 varImp(forestENEM, scale = T) # importância de cada variável
@@ -27,5 +27,6 @@ varImpPlot(forestENEM, type=2) # importância de cada variável
 predicaoForest  = predict(forestENEM, testeENEM) # criar predição
 predicaoCTree  = predict(cTreeENEM, testeENEM) # criar predição
 
-postResample(testeENEM[,4], predicaoForest) # teste de performance da Floresta Aleatória
-postResample(testeENEM[,4], predicaoCTree) # teste de performance da Árvore Condicional
+postResample(testeENEM[ , 4], predicaoForest) # teste de performance da Floresta Aleatória
+postResample(testeENEM[ , 4], predicaoCTree) # teste de performance da Árvore Condicional
+
