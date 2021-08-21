69746c0375f251569a55656b517b0ae5e4f37eb8
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-08-21 20:58:55 -0300

    atualização introdução

--- scripts/introducao/pacotes.R
+++ scripts/introducao/pacotes.R
@@ -8,3 +8,8 @@ library(caret)
 
 # carga temporária
 caret::featurePlot(x = iris[, 1:4], y = iris$Species) # plot das variáveis do conjunto de dados iris, usando o pacote caret
+
+# pacman
+install.packages('pacman')
+
+pacman::p_load(caret, VIM) # carrega se o pacote estiver instalado. Se não estiver, instala.
