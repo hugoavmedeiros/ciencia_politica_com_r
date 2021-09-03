a71691973f641ffb71466a2a2374bb7170763bd3
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-09-03 18:44:39 -0300

    atualização com plot de avaliação do modelo

--- scripts/machine_learning/regressao_com_cv_ENEM.R
+++ scripts/machine_learning/regressao_com_cv_ENEM.R
@@ -36,6 +36,11 @@ varImpPlot(ENEM_RF, type=2) # importância de cada variável
 # Boosting
 # Treinamento
 ENEM_ADA <- train(nota ~ tipo + TDI_03 + MHA_03, data = ENEM_ESCOLA_2019, method = "glmboost")
+
+# evolução do modelo
+plot(ENEM_ADA)
+
 # Sumários
 print(ENEM_ADA)
 summary(ENEM_ADA)
+
