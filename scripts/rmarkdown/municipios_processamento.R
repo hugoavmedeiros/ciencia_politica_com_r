193be6b1d8fbfd5c969ed2c5b5b59c2af583708e
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-09-18 10:24:07 -0300

    código com carga web

--- scripts/rmarkdown/municipios_processamento.R
+++ scripts/rmarkdown/municipios_processamento.R
@@ -5,12 +5,12 @@ padroniza <- function(x) {
 }
 
 # ETL
-mun_pe <- read.csv2('../../bases_tratadas/clusters_municipios_pe.csv')
+mun_pe <- read.csv2('https://raw.githubusercontent.com/hugoavmedeiros/cd_com_r/master/bases_tratadas/clusters_municipios_pe.csv')
 mun_pe$rd <- as.factor(mun_pe$rd)
 mun_pe$municipio <- as.factor(mun_pe$municipio)
 
 # mun_pe <- read.csv2('bases_tratadas/clusters_municipios_pe.csv')
 
-mun_pe_meta <- read.csv2('../../bases_tratadas/clusters_municipios_pe_meta.csv')
+mun_pe_meta <- read.csv2('https://raw.githubusercontent.com/hugoavmedeiros/cd_com_r/master/bases_tratadas/clusters_municipios_pe_meta.csv')
 
 # mun_pe_meta <- read.csv2('bases_tratadas/clusters_municipios_pe_meta.csv')
