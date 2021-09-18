c0226501f2b080e9f816e680d7fa43f2f435e08a
Hugo Medeiros <hugoavmedeiros@gmail.com>
2021-09-18 09:21:47 -0300

    atualização

--- scripts/rmarkdown/municipios_processamento.R
+++ scripts/rmarkdown/municipios_processamento.R
@@ -10,3 +10,7 @@ mun_pe$rd <- as.factor(mun_pe$rd)
 mun_pe$municipio <- as.factor(mun_pe$municipio)
 
 # mun_pe <- read.csv2('bases_tratadas/clusters_municipios_pe.csv')
+
+mun_pe_meta <- read.csv2('../../bases_tratadas/clusters_municipios_pe_meta.csv')
+
+# mun_pe_meta <- read.csv2('bases_tratadas/clusters_municipios_pe_meta.csv')
