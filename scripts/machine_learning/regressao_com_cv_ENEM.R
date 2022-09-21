a77d2c9aef5f049b7bf18bd498550150bd874699
Hugo Medeiros <hugo.vasconcelos@sectec.local>
2022-09-21 11:27:43 -0300

    atualização do scrip regressão com cv, contendo - partição treino e teste - cross-validation - criação de vários modelos - análise de diferentes modelos - predição - plot comparando predição e real - explicação e demonstração de MAE e RMSE

--- scripts/machine_learning/regressao_com_cv_ENEM.R
+++ scripts/machine_learning/regressao_com_cv_ENEM.R
@@ -1,8 +1,13 @@
 # carrega as bibliotecas
-pacman::p_load(car, caret, corrplot, data.table, dplyr, forcats, funModeling, mltools, randomForest, tidyverse)
+pacman::p_load(ade4, car, caret, corrplot, data.table, dplyr, forcats, funModeling, ggplot2, mlbench, mltools, randomForest, rattle, tidyverse)
 
-# carregar a base de dados
+# leitura da base de dados
 ENEM_ESCOLA_2019 <- read.csv2('https://raw.githubusercontent.com/hugoavmedeiros/etl_com_r/master/bases_tratadas/ENEM_ESCOLA_2019.csv', stringsAsFactors = T) # carregando a base já tratada para o ambiente do R
+
+ENEM_ESCOLA_2019_D <- acm.disjonctif(as.data.frame(ENEM_ESCOLA_2019$tipo))
+names(ENEM_ESCOLA_2019_D) <- c('EREM', 'ETE', 'Federal', 'Privada', 'Regular')
+
+ENEM_ESCOLA_2019 <- cbind(ENEM_ESCOLA_2019, ENEM_ESCOLA_2019_D)
 
 # AED 
 status(ENEM_ESCOLA_2019) # explorar a qualidade das variáveis
@@ -10,47 +15,64 @@ freq(ENEM_ESCOLA_2019) # explorar os fatores
 plot_num(ENEM_ESCOLA_2019) # exploração das variáveis numéricas
 profiling_num(ENEM_ESCOLA_2019) # estatísticas das variáveis numéricas
 
-corrplot(cor(treinoENEM[ , c(4:12)])) # correlação entre as variáveis
+corrplot(cor(ENEM_ESCOLA_2019[ , c(4:12)])) # correlação entre as variáveis
 
 # Treino e Teste: Pré-processamento
-particaoENEM = createDataPartition(1:nrow(ENEM_ESCOLA_2019), p=.7) # cria a partição 70-30
-treinoENEM = ENEM_ESCOLA_2019[particaoENEM$Resample1, ] # treino
-testeENEM = ENEM_ESCOLA_2019[-particaoENEM$Resample1, ] # - treino = teste
+particaoENEM = createDataPartition(ENEM_ESCOLA_2019$nota, p=.7, list = F) # cria a partição 70-30
+treinoENEM = ENEM_ESCOLA_2019[particaoENEM, ] # treino
+testeENEM = ENEM_ESCOLA_2019[-particaoENEM, ] # - treino = teste
 
 # Validação Cruzada: Pré-processamento
 # Controle de treinamento
-# train.control <- trainControl(method = "boot", number = 100)
 
-train.control <- trainControl(method = "cv", number = 10) # controle de treino
-# Treinamento
-ENEM_LM <- train(nota ~ tipo + TDI_03 + MHA_03, data = ENEM_ESCOLA_2019, method = "lm", trControl = train.control)
-# Sumários
-print(ENEM_LM)
-summary(ENEM_LM)
+train.control <- trainControl(method = "cv", number = 10, verboseIter = T) # controle de treino
 
-# Bagging para Regressão
-ENEM_RF = randomForest(treinoENEM[ , c(3, 8, 12)], treinoENEM[ , 4], ntree = 100, keep.forest=T, keep.inbag = TRUE, importance=T) # floresta aleatória
+# Treinamentos
+## Regressão Linear
+ENEM_LM <- train(nota ~ EREM + ETE + Federal + Privada + Regular + TDI_03 + MHA_03, data = treinoENEM, method = "lm", trControl = train.control)
+summary(ENEM_LM) # sumário do modelo linear
 
-plot(ENEM_RF)
+## Árvore de Decisão
+ENEM_RPART <- train(nota ~ EREM + ETE + Federal + Privada + Regular + TDI_03 + MHA_03, data = treinoENEM, method = "rpart", trControl = train.control)
 
+summary(ENEM_RPART)
+fancyRpartPlot(ENEM_RPART$finalModel) # desenho da árvore
+plot(varImp(ENEM_RPART)) # importância das variáveis
+
+# Bagging com Floresta Aleatória
+ENEM_RF <- train(nota ~ EREM + ETE + Federal + Privada + Regular + TDI_03 + MHA_03, data = treinoENEM, method = "cforest", trControl = train.control)
+
+plot(ENEM_RF) # evolução do modelo
 varImp(ENEM_RF, scale = T) # importância de cada variável
-varImpPlot(ENEM_RF, type=2) # importância de cada variável
+plot(varImp(ENEM_RF, scale = T)) # plot de importância
 
-# Boosting
-# Treinamento
-ENEM_ADA <- train(nota ~ tipo + TDI_03 + MHA_03, data = ENEM_ESCOLA_2019, method = "glmboost", trControl = train.control)
+# Boosting com Boosted Generalized Linear Model
+ENEM_ADA <- train(nota ~ EREM + ETE + Federal + Privada + Regular + TDI_03 + MHA_03, data = treinoENEM, method = "glmboost", trControl = train.control)
 
-# evolução do modelo
-plot(ENEM_ADA)
+plot(ENEM_ADA) # evolução do modelo
+print(ENEM_ADA) # modelo
+summary(ENEM_ADA) # sumário
 
-# Sumários
-print(ENEM_ADA)
-summary(ENEM_ADA)
+melhor_modelo <- resamples(list(LM = ENEM_LM, RPART = ENEM_RPART, RF = ENEM_RF, ADABOOST = ENEM_ADA))
+melhor_modelo
 
-# Bagging para Classificação
-IRIS_RF = randomForest(iris[ , 1:4], iris[ , 5], ntree = 100, keep.forest=T, keep.inbag = TRUE, importance=T) # floresta aleatória
+summary(melhor_modelo)
 
-plot(IRIS_RF)
+predVals <- extractPrediction(list(ENEM_RF), testX = testeENEM[, c(8, 12, 13:17)], testY = testeENEM$nota) 
 
-varImp(IRIS_RF, scale = T) # importância de cada variável
-varImpPlot(IRIS_RF, type=2) # importância de cada variável
+plotObsVsPred(predVals)
+
+# MAE = Mean absolute erro (Erro médio absoluto) - Média dos módulos dos resíduos
+# RMSE = Root Mean Squared Error (Raiz quadrada do erro-médio) - Média da raiz quadrada do quadrado do resíduo
+  # Semelhanças
+    # Mesma escala da variável de interesse
+    # Quanto menor melhor (orientadas negativamente)
+  # Diferenças
+    # RMSE capta melhor a variância dos erros
+    # MAE é mais simples de interpretar 
+
+plot(erros1 <- c(rep(2,10)))
+plot(erros2 <- c(rep(1,5), rep(3,5)))
+plot(erros3 <- c(rep(0,8), rep(10,2)))
+lista_erros <- matrix(c(sum(erros1)/10,sum(erros2)/10, sum(erros3)/10, sqrt(sum(erros1^2)/10), sqrt(sum(erros2^2)/10), sqrt(sum(erros3^2)/10)), ncol = 2, dimnames = list(c('Erros 1', 'Erros 2', 'Erros 3'), c('MAE', 'RMSE')))
+lista_erros
