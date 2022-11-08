e840231779b83f77af1e1ca5e6615b374272d0d1
Hugo Medeiros <hugo.vasconcelos@sectec.local>
2022-11-07 22:06:39 -0300

    simplificação do código, com retirada do ggplot

--- scripts/machine_learning/agrupamento_kmeans_iris.R
+++ scripts/machine_learning/agrupamento_kmeans_iris.R
@@ -5,10 +5,10 @@ pacman::p_load(cluster, dplyr, factoextra, ggplot2)
 iris_cluster <- iris[ , -5]
 str(iris_cluster)
 
-# setar semente aleatória
+# definir semente aleatória
 set.seed(1)
 
-# elbow method
+# Método do Cotovelo
 fviz_nbclust(iris_cluster, kmeans, method = "wss")
 
 # Agrupamento com kmeans
@@ -16,28 +16,5 @@ cls <- kmeans(x = iris_cluster, centers = 3) # aprendizagem ns
 iris_cluster$cluster <- as.factor(cls$cluster) # passamos os clusters para a base original
 head(iris_cluster)
 
-# plot nativo do R
-plot(cls)
-
 # plot com função própria do pacote
 clusplot(iris_cluster, cls$cluster, xlab = 'Fator1', ylab = 'Fator2', main = 'Agrupamento Estudantes', lines = 0, shade = F, color = TRUE, labels = 2)
-
-# plot com ggplot
-ggplot() +
-  geom_point(data = iris_cluster, 
-             mapping = aes(x = Sepal.Length, 
-                           y = Petal.Length, 
-                           colour = cluster)) +
-  geom_point(mapping = aes_string(x = cls$centers[ , "Sepal.Length"], 
-                                  y = cls$centers[ , "Petal.Length"]),
-             color = "red", size = 4) +
-  geom_text(mapping = aes_string(x = cls$centers[ , "Sepal.Length"], 
-                                 y = cls$centers[ , "Petal.Length"],
-                                 label = 1:3),
-            color = "white", size = 2) +
-  theme_light()
-
-## comparação
-iris_cluster$Species <- iris$Species
-
-iris_cluster %>% count(Species, cluster, sort = TRUE)
