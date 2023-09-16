acd17b5f9085e2f7cba3e24ad714f2336ee8d803
Hugo Medeiros <hugoavmedeiros@gmail.com>
2023-09-16 08:56:20 -0300

    atualização script árvores de decisão

--- scripts/machine_learning/arvores_decisao_introducao.R
+++ scripts/machine_learning/arvores_decisao_introducao.R
@@ -1,6 +1,6 @@
 # pacotes
 pacman::p_load(
-  caret, 
+  caret, rattle
 ) 
 
 # Github
@@ -13,9 +13,18 @@ testeENEM = ENEM_ESCOLA_2019[-particaoENEM$Resample1, ] # - treino = teste
 
 # Mineração e predição com Árvores de Decisão
 ## Árvore de Decisão
-ENEM_RPART <- train(nota ~ tipo + ICG + TDI_03 + MHA_03, data = treinoENEM, method = "rpart", trControl = train.control)
+ENEM_RPART <- train(
+  nota ~ tipo + ICG + TDI_03 + MHA_03, 
+  data = treinoENEM, 
+  method = "rpart", 
+  trControl = trainControl(method = "cv"))
 
 fancyRpartPlot(ENEM_RPART$finalModel) # desenho da árvore
 plot(varImp(ENEM_RPART)) # importância das variáveis
 
 varImp(ENEM_RPART, scale = T) # importância de cada variável
+
+predicaoTree = predict(ENEM_RPART, newdata = testeENEM)
+
+postResample(testeENEM[ , 7], predicaoTree) # teste de performance da Árvore Condicional
+
