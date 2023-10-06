548a07a70c10a057af16f5142b18decb74cf0cd6
Hugo Medeiros <hugoavmedeiros@gmail.com>
2023-10-06 18:48:12 -0300

    atualização dos scripts de big data

--- scripts/etl/large_data.R
+++ scripts/etl/large_data.R
@@ -1,35 +1,91 @@
-pacman::p_load(ff)
+##### ARMAZENAMENTO EM DISCO #####
+# install_github("edwindj/ffbase", subdir="pkg")
+pacman::p_load(biglm, devtools, dplyr, ff, ffbase)
 
 enderecoBase <- 'bases_originais/largeData.csv'
 
 # criando o arquivo ff
-system.time(extracaoLD4 <- read.csv.ffdf(file=enderecoBase))
+tempo_ff <- system.time(base_ff <- read.csv.ffdf(file=enderecoBase))
 
-class(extracaoLD4) # veja a classe do objeto
+tempo_ff
+
+base_ff %>% head()
+
+base_ff %>% typeof() # veja a classe do objeto
+
+base_ff %>% class() # veja a classe do objeto
  
-object.size(extracaoLD3) # a vantagem está no tamanho!
+base_ff %>% object.size() # a vantagem está no tamanho!
 
-object.size(extracaoLD4) # a vantagem está no tamanho!
+sum(base_ff[,3]) # algumas operações são possíveis diretamente
 
-sum(extracaoLD4[,3]) # algumas operações são possíveis diretamente
+# REGRESSÂO #
 
-lm(c ~ ., extracaoLD4) ## não vai rodar!!!! o vetor de computação será mt grande
+lm(c ~ ., base_ff) ## não vai rodar!!!! o vetor de computação será mt grande
 
-# pra outras, será preciso amostrar...
-extracaoLD4Amostra <- extracaoLD4[sample(nrow(extracaoLD4), 100000) , ]
+# mas pode ser feita com amostragem
+base_ffAmostra <- base_ff[sample(nrow(base_ff), 100000) , ]
 
-lm(c ~ ., extracaoLD4Amostra) # aí, funciona!!!
+lm(c ~ ., base_ffAmostra) # aí, funciona!!!
 
-system.time(extracaoLD5 <- read.csv.ffdf(file='bases_originais/Phones_accelerometer.csv'))
+# ou com funções otimizadas
+modelo <- biglm(a ~ b + c,  data = base_ff)
 
-length(extracaoLD5$Model)
+summary(modelo)
 
-system.time(extracaoLD6 <- read.csv.ffdf(file='bases_originais/Phones_gyroscope.csv'))
+##### ARROW #####
+pacman::p_load(arrow, dplyr)
 
-length(extracaoLD6$Model)
+enderecoBase <- 'bases_originais/largeData.csv'
 
-extracaoMerge <- ffdfappend(extracaoLD5, extracaoLD6) # junta bases semelhantes
+# criando o arquivo ff
+tempo_arrow <- (system.time(base_arrow <- read_csv_arrow(file=enderecoBase)))
 
-#extracaoMerge <- ffdfrbind.fill(extracaoLD5, extracaoLD6) # junta bases semelhantes forçando preenchimento
+base_arrow %>% head()
 
-length(extracaoMerge$Model)
+base_arrow %>% typeof()
+
+base_arrow %>% class()
+
+base_arrow %>% object.size() # não ha vantagem no tamanho
+
+base_arrow_t <- arrow_table(base_arrow)
+
+base_arrow_t %>% typeof()
+
+base_arrow_t %>% class()
+
+base_arrow_t %>% object.size() # não ha vantagem no tamanho
+
+base_arrow_t
+
+base_arrow_t %>%
+  group_by(d) %>%
+  summarize(
+    mean_a = mean(a),
+    mean_b = mean(b),
+    total = n()) %>%
+  filter(mean_a > 0) %>%
+  arrange(mean_a) %>%
+  collect()
+
+base_arrow_s1 <- base_arrow %>% sample_n(500000, replace = TRUE) %>% compute()
+
+base_arrow_s1 %>% typeof()
+
+base_arrow_s1 %>% class()
+
+base_arrow_s1 %>% object.size()
+
+base_arrow_s1_mod <- lm(a ~ b + c + d + e + f + g, base_arrow_s1)
+
+summary(base_arrow_s1_mod)
+
+#### SPARK ####
+# spark_install("3.5")
+
+# install.packages("sparklyr")
+
+# packageVersion("sparklyr")
+
+# install.packages("https://cran.r-project.org/src/contrib/Archive/SparkR/SparkR_2.3.0.tar.gz", repos = NULL, type="source")
--- scripts/etl/large_data_criacao.R
+++ scripts/etl/large_data_criacao.R
@@ -1,16 +1,43 @@
 # install.packages('data.table')
-library(data.table)
+pacman::p_load(data.table)
 
 casos= 2e7 # reduza os números antes e depois do e, caso esteja difícil de computar # mas tente manter pelo menos 1e6, para garantir o efeito se large data
 
 # cria o data.frame com o total de casos definido acima
-largeData = data.table(a=rpois(casos, 3), b=rbinom(casos, 1, 0.7), c=rnorm(casos), d=sample(c("fogo","agua","terra","ar"), casos, replace=TRUE), e=rnorm(casos), f=rpois(casos, 3), g=rnorm(casos))
+largeData = data.table(
+  a = rpois(casos, 3), 
+  b = rbinom(casos, 1, 0.7), 
+  c = rnorm (casos), 
+  d = sample(c("fogo","agua","terra","ar"), casos, replace=TRUE), 
+  e = rnorm(casos), 
+  f = rpois(casos, 3), 
+  g = rnorm(casos))
 
 object.size(largeData) # retorna o tamanho do objeto
 
 head(largeData) # vê as primeiras linhas
 
 write.table(largeData,"bases_originais/largeData.csv",sep=",",row.names=FALSE,quote=FALSE) # salva em disco
+
+### base correlacionada ###
+# cria o data.frame com o total de casos definido acima
+a = rpois(casos, 3)
+e = rnorm(casos)
+
+largeData2 = data.table(
+  a = a, 
+  b = 10 + 0.5*a,
+  c = sqrt(a),
+  d = sample(c("fogo","agua","terra","ar"), casos, replace=TRUE), 
+  e = e, 
+  f = a - e, 
+  g = e^2 +2*a)
+
+object.size(largeData2) # retorna o tamanho do objeto
+
+head(largeData2) # vê as primeiras linhas
+
+write_parquet(largeData2,"bases_originais/largeData2.parquet") # salva em disco
 
 # versão menor
 
