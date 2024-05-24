20e50776d73fbeffda4365b47dd19d88075d1ff9
Hugo Medeiros <hugoavmedeiros@gmail.com>
2024-05-24 17:50:21 -0300

    Add files via upload

--- scripts/machine_learning/regras_associacao_TSE.R
+++ scripts/machine_learning/regras_associacao_TSE.R
@@ -1,7 +1,7 @@
 ##### Regras de Associação 
 pacman::p_load(
   # ETL
-  data.table, dplyr,
+  data.table, dplyr, janitor,
   # MACHINE LEARNING
   caret,
   # REGRAS DE ASSOCIAÇÃO
@@ -11,31 +11,31 @@ pacman::p_load(
 )
 
 ##### ETL #####
-candidatos_pe_2022 <- fread('bases_originais/consulta_cand_2022_PE.csv', encoding = 'Latin-1', stringsAsFactors = T)
+candidatos_pe_2022 <- fread('https://raw.githubusercontent.com/hugoavmedeiros/ciencia_politica_com_r/master/bases_originais/consulta_cand_2022_PE.csv', encoding = 'Latin-1', stringsAsFactors = T) %>%
+  janitor::clean_names()
 
 # filtrar apenas deputados estaduais e variáveis de perfil
-estaduais_pe_2022 <- candidatos_pe_2022 %>% filter(DS_CARGO == 'DEPUTADO ESTADUAL') %>% select(TP_AGREMIACAO, NM_MUNICIPIO_NASCIMENTO, NR_IDADE_DATA_POSSE, DS_GENERO, DS_GRAU_INSTRUCAO, DS_ESTADO_CIVIL, DS_COR_RACA, DS_OCUPACAO)
+estaduais_pe_2022 <- candidatos_pe_2022 %>% 
+  filter(ds_cargo == 'DEPUTADO ESTADUAL') %>% 
+  select(tp_agremiacao, nm_municipio_nascimento, nr_idade_data_posse, ds_genero, ds_grau_instrucao, ds_estado_civil, ds_cor_raca, ds_ocupacao)
 
 # observar se os dados estão com as classes certas
-glimpse(estaduais_pe_2022)
-
-# modificar nomes para facilitar análise e plot
-names(estaduais_pe_2022) <- c(
-  'agremiacao', 'municipio', 'idade', 'genero', 'instrucao', 'estado_civil', 'raca', 'ocupacao'
-)
+estaduais_pe_2022 %>% glimpse()
 
 # discretizar variável numérica
 estaduais_pe_2022[ , 3] <- discretizeDF(estaduais_pe_2022[ , 3]) # transforma variáveis numéricas em fatores
 
 ##### MINERAÇÃO #####
 # mineração com a priori
-regras_estaduais <- apriori(
-  estaduais_pe_2022, 
-  parameter = list(supp = 0.2, conf = 0.5, minlen = 2, maxlen = 5))
+regras_estaduais <- estaduais_pe_2022 %>% 
+  apriori(parameter = list(supp = 0.2, conf = 0.5, minlen = 2, maxlen = 5))
 
-## limpar e organizar regras regras
+## limpar e organizar regras
 # três casas decimais
-quality(regras_estaduais) <- round(quality(regras_estaduais), digits = 3) 
+regras_estaduais <- regras_estaduais %>% 
+  quality() %>% 
+  round(3)
+
 # organizar por lift
 regras_estaduais <- sort(regras_estaduais, by="lift") 
 # remover regras redundantes
@@ -60,20 +60,3 @@ plot(regras_estaduais_res, method="paracoord", control=list(reorder=T), measure=
 
 # gráfico de relações agrupadas
 plot(regras_estaduais_res, method="grouped", measure=c("lift"))
-
-### APENAS BRANCOS ###
-
-regras_brancos <- apriori(estaduais_pe_2022, control=list(verbose=F), parameter = list(minlen=2, supp=0.1, conf=0.3), appearance = list(lhs="raca=BRANCA", default="rhs"))
-
-quality(regras_brancos)<-round(quality(regras_brancos),digits = 3)
-regras_brancos<-sort(regras_brancos,by="lift")
-
-regras_brancos_df = data.frame(
-  lhs = labels(lhs(regras_brancos)),
-  rhs = labels(rhs(regras_brancos)), 
-  regras_brancos@quality)
-
-reactable(regras_brancos_df, defaultColDef = colDef( cell = data_bars(regras_brancos_df,text_position = 'outside-base')
-))
-
-plot(regras_brancos,method="paracoord",control=list(reorder=T),measure=c("lift"),lty = "dotted")
